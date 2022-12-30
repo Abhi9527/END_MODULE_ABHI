e81536f3b514f0cfd18e286726e69ab4fe4ca645
dac <dac@FEB89>
2022-12-30 16:04:17 +0530

    Abhi First Code

---
+++ server.js
@@ -0,0 +1,7 @@
+const express= require("express");
+const app = express();
+app.get('/',function(req,resp){
+resp.send("Hello World");
+});
+app.listen(4000,function(){
+    console.log("server running on 4000")});
